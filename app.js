7a535943943fa9c37992cbd1d1e8ea7ead7ab621
spencerwooo <spencerwoo98@foxmail.com>
2018-11-23 17:01:54 +0800

    😊 Add token refresh unit

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var axios = require('axios')
 
 var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users')
 var apiRouter = require('./routes/api')
+var token = require('./storage/token.js')
 
 var app = express()
 
@@ -39,5 +41,28 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500)
   res.render('error')
 })
+
+// refresh tokens every 10 minutes
+var jikeTokenRefreshUrl = 'https://app.jike.ruguoapp.com/app_auth_tokens.refresh'
+var refreshRate = 10 * 60 * 1000
+
+function refreshTokens () {
+  console.log('Started background task: refresh tokens...')
+  setInterval(function () {
+    axios({
+      method: 'GET',
+      url: jikeTokenRefreshUrl,
+      headers: {
+        'x-jike-refresh-token': token.token.refreshToken
+      }
+    }).then(function (response) {
+      console.log(response.data)
+    }).catch(function (error) {
+      console.log(error)
+    })
+  }, refreshRate)
+}
+
+refreshTokens()
 
 module.exports = app
--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,15 @@
 var express = require('express')
+var axios = require('axios')
 var router = express.Router()
+var token = require('../storage/token.js')
 
-var response = {
-  status: 'ok',
-  uuid: '10.0.0.55'
+var status = {
+  ok: 'ok',
+  err: 'error'
 }
+
+var jikeAccessToken = token.token.accessToken
+var jikeApiRootUrl = 'https://app.jike.ruguoapp.com/1.0/users/profile?username='
 
 router.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
@@ -12,8 +17,41 @@ router.use(function (req, res, next) {
   next()
 })
 
-router.get('/', function (req, res, next) {
-  res.json(response)
+router.get('/', function (req, res) {
+  res.json(status.ok)
+})
+
+router.get('/username/:jikeUUID', function (req, res) {
+  var username = req.params.jikeUUID
+  // res.json(req.params)
+
+  axios({
+    method: 'GET',
+    url: jikeApiRootUrl + username,
+    headers: {
+      'x-jike-access-token': jikeAccessToken
+    }
+  }).then(function (response) {
+    // console.log(response.data)
+    var userInfo = response.data.user
+    var user = {
+      screenName: userInfo.screenName,
+      bio: userInfo.bio,
+      isVerified: userInfo.isVerified,
+      verifyMessage: userInfo.verifyMessage,
+      avatarImage: userInfo.avatarImage.smallPicUrl,
+      statsCount: {
+        followed: userInfo.statsCount.followedCount,
+        following: userInfo.statsCount.followingCount
+      },
+      createdTime: userInfo.createdAt
+      // playgrounds: ['', '', '']
+    }
+    res.json(user)
+  }).catch(function (error) {
+    console.log(error)
+    res.send(status.err)
+  })
 })
 
 module.exports = router
